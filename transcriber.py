4507fd34853fb3a88f9b4f278c734a1d7bac8138
KostasEreksonas <k.ereksonas@gmail.com>
2022-10-04 22:12:27 +0300

    Supply URL when running the script

--- transcriber.py
+++ transcriber.py
@@ -5,12 +5,12 @@ import subprocess
 import whisper
 import sys
 
+filename = "audio.mp3"
+
 # Download mp3 audio of a Youtube video. Credit to Stokry
 # https://dev.to/stokry/download-youtube-video-to-mp3-with-python-26p
 def audio():
-    video_url = input("Enter Youtube URL: ")
-    video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
-    filename = "audio.mp3"
+    video_info = youtube_dl.YoutubeDL().extract_info(url = sys.argv[1],download=False)
     options={
         'format':'bestaudio/best',
         'keepvideo':False,
@@ -25,7 +25,7 @@ def main():
     # Select speech recognition model
     model = input("Select speech recognition model (tiny, base, small, medium, large): ")
     model = whisper.load_model(model)
-    result = model.transcribe("audio.mp3")
+    result = model.transcribe(filename)
     file = open('transcription.txt', 'w')
     file.write(result["text"])
     file.write("\n")
