66c2e6d172f4dc3a4bfb982900181b2f7b546847
KostasEreksonas <k.ereksonas@gmail.com>
2022-10-05 11:18:01 +0300

    Added a check for CUDA; cleaned up code

--- transcriber.py
+++ transcriber.py
@@ -3,9 +3,10 @@
 import youtube_dl
 import subprocess
 import whisper
+import torch
 import sys
 
-filename = "audio.mp3"
+audiofile = "audio.mp3"
 
 # Download mp3 audio of a Youtube video. Credit to Stokry
 # https://dev.to/stokry/download-youtube-video-to-mp3-with-python-26p
@@ -14,22 +15,32 @@ def audio():
     options={
         'format':'bestaudio/best',
         'keepvideo':False,
-        'outtmpl':filename,
+        'outtmpl':audiofile,
     }
 
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
 
+# Check wether CUDA is available or not
+def checkDevice():
+    if (torch.cuda.is_available() == 1):
+        DEVICE = "cuda"
+    else:
+        DEVICE = "cpu"
+    return DEVICE
+
 def main():
     audio() # Download an mp3 audio file to transcribe to text
-    # Select speech recognition model
-    model = input("Select speech recognition model (tiny, base, small, medium, large): ")
-    model = whisper.load_model(model)
-    result = model.transcribe(filename)
-    file = open('transcription.txt', 'w')
-    file.write(result["text"])
-    file.write("\n")
-    file.close
+
+	# Select speech recognition model
+    name = input("Select speech recognition model name (tiny, base, small, medium, large): ")
+    model = whisper.load_model(name,device=checkDevice())
+
+    # Save transcribed text to file
+    result = model.transcribe(audiofile)
+    with open('transcription.txt', 'a') as file:
+        file.write(result["text"])
+        file.write("\n")
 
 if __name__ == "__main__":
     main()
