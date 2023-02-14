d9067dae8a98829deff3a5acb04640f56a8586b7
KostasEreksonas <k.ereksonas@gmail.com>
2023-02-14 10:46:58 +0200

    Updated readability of code and moved parts of it to functions

--- transcriber.py
+++ transcriber.py
@@ -15,7 +15,7 @@ audiofile = "audio.mp3" # Save audio file as audio.mp3
 
 # Download mp3 audio of a Youtube video. Credit to Stokry
 # https://dev.to/stokry/download-youtube-video-to-mp3-with-python-26p
-def audio():
+def getAudio():
     url = None
     argv = sys.argv[1:]
     try:
@@ -45,30 +45,37 @@ def checkDevice():
         DEVICE = "cpu"
     return DEVICE
 
-def main():
-    audio() # Download an mp3 audio file to transcribe to text
+def translateResult(orgFile, transFile, text):
+    # Translate transcribed text. Credit to Harsh Jain at educative.io
+    # https://www.educative.io/answers/how-do-you-translate-text-using-python
+    translator = Translator() # Create an instance of Translator() class
+    with open(orgFile, 'r') as transcription:
+        contents = transcription.read()
+        translation = translator.translate(contents)
+    with open(transFile, 'a') as file:
+        file.write(translation.text)
 
+# Put a newline character after each sentence
+def formatResult(fileName, text):
+    formatText = re.sub('\.', '.\n', text)
+    with open(fileName, 'a') as file:
+        file.write(formatText)
+        choice = input("Do you want to translate audio transcription to English? (Yes/No) ")
+    if (choice == "Yes"):
+        translateResult('transcription.txt', 'translation.txt', formatText)
+
+def getResult():
 	# Select speech recognition model
-    name = input("Select speech recognition model name (tiny, base, small, medium, large): ")
-    model = whisper.load_model(name,device=checkDevice())
+    modelName = input("Select speech recognition model name (tiny, base, small, medium, large): ")
+    model = whisper.load_model(modelName,device=checkDevice())
     # Save transcribed text to file
     result = model.transcribe(audiofile)
-    edited_transcription = re.sub('\.', '.\n', result["text"]) # Put a newline character after each sentence
-    with open('transcription.txt', 'a') as file:
-        file.write(edited_transcription)
-        file.write("\n")
+    formatResult('transcription.txt', result["text"])
 
-    choice = input("Do you want to translate audio transcription to English? (Yes/No) ")
-    if (choice == "Yes"):
-        # Translate transcribed text. Credit to Harsh Jain at educative.io
-        # https://www.educative.io/answers/how-do-you-translate-text-using-python
-        translator = Translator() # Create an instance of Translator() class
-        with open('transcription.txt', 'r') as transcription:
-            contents = transcription.read()
-            translation = translator.translate(contents)
-            edited_translation = re.sub('\.', '.\n', translation.text) # Put a newline character after each sentence
-        with open('translation.txt', 'a') as file:
-            file.write(edited_translation)
+def main():
+    getAudio() # Download an mp3 audio file to transcribe to text
+    getResult() # Get transcription
+
 
 if __name__ == "__main__":
     main()
