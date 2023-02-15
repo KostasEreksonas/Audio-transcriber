584906077d5be844e385ad7e26b0b45c9bcd8395
KostasEreksonas <k.ereksonas@gmail.com>
2023-02-15 22:29:48 +0200

    Cleaned up code after git merge

--- transcriber.py
+++ transcriber.py
@@ -38,10 +38,7 @@ def get_audio():
         print("Usage: python3 transcriber.py -u <url>")
     for opt, arg in opts:
         if opt in ['-u', '--url']:
-            url = arg
-            match = re.search("shorts/", url)
-            if bool(match):
-                url = re.sub("shorts/", "watch?v=", url)
+            url = match_pattern("shorts/", arg)
     video_info = youtube_dl.YoutubeDL().extract_info(url=url, download=False)
     options = {
         'format': 'bestaudio/best',
@@ -69,6 +66,7 @@ def check_device():
 def get_result():
     """Get speech recognition model."""
     model_name = input("Select speech recognition model name (tiny, base, small, medium, large): ")
+    banner("Transcribing text")
     model = whisper.load_model(model_name, device=check_device())
     result = model.transcribe(AUDIOFILE)
     format_result('transcription.txt', result["text"])
@@ -78,10 +76,11 @@ def format_result(file_name, text):
     """Put a newline character after each sentence and prompt user for translation."""
     format_text = re.sub('\.', '.\n', text)
     with open(file_name, 'a', encoding="utf-8") as file:
+        banner("Writing transcription to text file")
         file.write(format_text)
         choice = input("Do you want to translate audio transcription to English? (Yes/No) ")
     if choice == "Yes":
-        translate_result('transcription.txt', 'translation.txt', format_text)
+        translate_result('transcription.txt', 'translation.txt')
 
 
 def translate_result(org_file, trans_file):
@@ -92,8 +91,10 @@ def translate_result(org_file, trans_file):
     translator = Translator()  # Create an instance of Translator() class
     with open(org_file, 'r', encoding="utf-8") as transcription:
         contents = transcription.read()
+        banner("Translating text")
         translation = translator.translate(contents)
     with open(trans_file, 'a', encoding="utf-8") as file:
+        banner("Writing translation to text file")
         file.write(translation.text)
 
 
