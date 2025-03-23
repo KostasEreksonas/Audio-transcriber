620afd292de34f8b282712bbaaa0c6fa4cc5266b
KostasEreksonas <k.ereksonas@gmail.com>
2025-03-23 19:35:10 +0200

    Updated transcription to full length of a given audio

--- transcriber.py
+++ transcriber.py
@@ -9,10 +9,10 @@ model = whisper.load_model(sys.argv[1])
 
 # load audio and pad/trim it to fit 30 seconds
 audio = whisper.load_audio("outputs/audio.mp3")
-audio = whisper.pad_or_trim(audio)
+audio_trim = whisper.pad_or_trim(audio)
 
 # make log-Mel spectrogram and move to the same device as the model
-mel = whisper.log_mel_spectrogram(audio, n_mels=model.dims.n_mels).to(model.device)
+mel = whisper.log_mel_spectrogram(audio_trim, n_mels=model.dims.n_mels).to(model.device)
 
 # detect the spoken language and write to results file
 _, probs = model.detect_language(mel)
@@ -20,15 +20,15 @@ language_detected = max(probs, key=probs.get)
 print(f"Detected language: {language_detected}")
 
 with open(result_file, "a") as file:
-    file.write(f"Detected language: {language_detected}\n")
+    file.write(f"\nDetected language: {language_detected}\n")
 
 # decode the audio
-options = whisper.DecodingOptions()
-result = whisper.decode(model, mel, options)
+result = model.transcribe(audio)
 
 # write the recognized text to file
 with open(result_file, "a") as file:
     file.write("\nTranscription:\n")
-    file.write(result.text)
+    file.write(result["text"])
+    file.write("\n")
 
 print(f"Transcription saved at: {result_file}")
